bdd3d91b643338fb809779a9a85e6b6200e5332d
Pritimoy Paul <pritimoy97@gmail.com>
2024-05-15 22:32:47 +0600

    image upload complete

--- screens/details.js
+++ screens/details.js
@@ -4,12 +4,15 @@ import {
   TouchableOpacity,
   useWindowDimensions,
   Image,
+  ToastAndroid,
 } from "react-native";
 import React, { useEffect, useState } from "react";
 import { doc, getDoc, getFirestore, updateDoc } from "firebase/firestore";
 import { firebaseApp } from "../firebaseConfig";
 import { useStore } from "../store";
 import uploadImageFromDevice from "../utils/uploadImageFromDevice";
+import getBlobFromUri from "../utils/getBlobFromUri";
+import { getDownloadURL, getStorage, ref, uploadBytes } from "firebase/storage";
 
 const DetailsScreen = ({ navigation, route }) => {
   const db = getFirestore(firebaseApp);
@@ -18,6 +21,7 @@ const DetailsScreen = ({ navigation, route }) => {
   const [data, setData] = useState({});
   const { width } = useWindowDimensions();
   const [imgURI, setImageURI] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   //get local file
   const handleLocalImageUpload = async () => {
@@ -53,6 +57,42 @@ const DetailsScreen = ({ navigation, route }) => {
     setUpdatedTime(updatedTime + 1);
   };
 
+  const addImageToTask = async (fileUrl) => {
+    const docRef = doc(db, "task", data.id);
+    await updateDoc(docRef, {
+      image: fileUrl,
+    });
+    getData();
+    setUpdatedTime(updatedTime + 1);
+    setImageURI(null);
+    ToastAndroid.show("Image Updated!", ToastAndroid.SHORT);
+  };
+
+  const handleCloudImageUpload = async () => {
+    if (!imgURI) return;
+
+    let fileToUpload = null;
+
+    const blob = await getBlobFromUri(imgURI);
+
+    const imgName = "img-" + new Date().getTime();
+
+    const storage = getStorage(firebaseApp, "gs://native-expo-2.appspot.com");
+
+    const storageRef = ref(storage, `images/${imgName}.jpg`);
+
+    setIsLoading(true);
+    await uploadBytes(storageRef, blob)
+      .then((snapshot) => {
+        return getDownloadURL(snapshot.ref);
+      })
+      .then((downloadURL) => {
+        console.log("Uploaded file : " + JSON.stringify(downloadURL));
+        addImageToTask(downloadURL);
+        setIsLoading(false);
+      });
+  };
+
   // useEffect
   useEffect(() => {
     console.log(route.params?.id);
@@ -76,7 +116,7 @@ const DetailsScreen = ({ navigation, route }) => {
       </View>
       <Text className="text-xl mt-4">{data?.text}</Text>
       <Text>{data?.description}</Text>
-      {imgURI && (
+      {imgURI ? (
         <View className="mt-4">
           <Image
             className="rounded-lg"
@@ -85,13 +125,36 @@ const DetailsScreen = ({ navigation, route }) => {
             style={{ width: width - 48, height: width / 1 }}
           />
         </View>
+      ) : data?.image ? (
+        <View className="mt-4">
+          <Image
+            className="rounded-lg"
+            source={{ uri: data?.image }}
+            resizeMode="contain"
+            style={{ width: width - 48, height: width / 1 }}
+          />
+        </View>
+      ) : (
+        ""
       )}
-      <TouchableOpacity
-        className="mt-4 bg-blue-900 p-3 rounded-sm"
-        onPress={() => handleLocalImageUpload()}
-      >
-        <Text className="text-white text-center">Update Image</Text>
-      </TouchableOpacity>
+      {imgURI ? (
+        <TouchableOpacity
+          className="mt-4 bg-blue-900 p-3 rounded-sm"
+          onPress={() => handleCloudImageUpload()}
+        >
+          <Text className="text-white text-center">
+            {isLoading ? "Loading..." : "Upload Image"}
+          </Text>
+        </TouchableOpacity>
+      ) : (
+        <TouchableOpacity
+          className="mt-4 bg-blue-900 p-3 rounded-sm"
+          onPress={() => handleLocalImageUpload()}
+        >
+          <Text className="text-white text-center">Choose Image</Text>
+        </TouchableOpacity>
+      )}
+
       <TouchableOpacity
         className="mt-4 bg-black p-3 rounded-sm"
         onPress={() => updateData(data.id, data.isDone)}
